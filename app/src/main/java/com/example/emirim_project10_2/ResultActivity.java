5930297338c3fecfdae80435fba6597121820364
dana8 <w2008@e-mirim.hs.kr>
2021-07-19 15:44:36 +0900

    Edit MainActivity(Check Votecount)

---
+++ app/src/main/java/com/example/emirim_project10_2/ResultActivity.java
@@ -0,0 +1,22 @@
+package com.example.emirim_project10_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.util.logging.LogManager;
+
+public class ResultActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+
+    }
+}
